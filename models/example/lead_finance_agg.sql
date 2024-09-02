38bccb09371723835f441f7edfe97840a391605e
Frank Steiner <auqifer.wave@gmail.com>
2024-09-02 07:19:02 +0000

    Start

---
+++ models/example/lead_finance_agg.sql
@@ -0,0 +1,8 @@
+SELECT 
+    offer AS Offer,
+    DELIVERED_ITEMS AS Delivered_Items,
+    CONFIRMED AS Confirmed,
+    DELIVERED AS Delivered,
+    total AS Total
+FROM 
+    smooth-topic-238416.lead_server_dataset.lead_server_finance_agg
